0bd1f2f287f1c81a037e56574808e98453d2b1fd
‘甘连’ <228400389@qq.com>
2026-01-09 11:20:44 +0800

    fix: 重新添加版本管理

--- src/api/login.ts
+++
@@ -1,9 +0,0 @@
-import { post } from '@/utils/request';
-
-/**
- * 用户登录
- * @param loginForm 登录表单
- */
-export function login(loginForm) {
-    return post('/auth/login', loginForm);
-}
--- src/typings.ts
+++
@@ -1,6 +0,0 @@
-// 枚举定义
-
-export enum TestEnum {}
-
-// uni.uploadFile文件上传参数
-export interface IUniUploadFileOptions {}
